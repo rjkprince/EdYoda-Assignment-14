166d0d53f6a5a05cc576892789aff8dc36677718
Rajesh kumar <prince.rk.cool@gmail.com>
2020-09-20 16:42:34 +0530

    Updates

---
+++ precache-manifest.36463c0c8d01a16b668096972e3844c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad30a2e422fa8437fe3f237b97c1404",
+    "url": "/stunning-react-app/index.html"
+  },
+  {
+    "revision": "9a4cb4ef6b2a8499b47c",
+    "url": "/stunning-react-app/static/css/main.12003038.chunk.css"
+  },
+  {
+    "revision": "2e07ef9833cc4a275ed9",
+    "url": "/stunning-react-app/static/js/2.055edf5c.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/stunning-react-app/static/js/2.055edf5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4cb4ef6b2a8499b47c",
+    "url": "/stunning-react-app/static/js/main.b2baac16.chunk.js"
+  },
+  {
+    "revision": "4cd9091aca77e735b6c1",
+    "url": "/stunning-react-app/static/js/runtime-main.4c6cb206.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a083884ce065544479a7f15cc12cf7b.js"
+  "/stunning-react-app/precache-manifest.36463c0c8d01a16b668096972e3844c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stunning-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
